d276e3adffba1e8e8f6a16d4d51d915a4766e7e7
Eric Wang <skygragon@gmail.com>
2018-04-14 11:15:01 +0800

    [solution] update with latest leetcode.com change
    refs #9
    refs #11
    
    Signed-off-by: Eric Wang <skygragon@gmail.com>

--- plugins/solution.discuss.js
+++ plugins/solution.discuss.js
@@ -12,33 +12,45 @@ var session = require('../session');
 //
 // https://github.com/skygragon/leetcode-cli-plugins/blob/master/docs/solution.discuss.md
 //
-var plugin = new Plugin(200, 'solution.discuss', '2017.12.21',
+var plugin = new Plugin(200, 'solution.discuss', '2018.04.14',
     'Plugin to fetch most voted solution in discussions.');
 
-var URL_DISCUSSES = 'https://discuss.leetcode.com/api/category/';
-var URL_DISCUSS_TOPIC = 'https://discuss.leetcode.com/topic/';
-var URL_DISCUSS_TOPIC_API = 'https://discuss.leetcode.com/api/topic/';
-
-function getSolutionDetail(solution, cb) {
-  if (!solution) return cb();
-
-  request(URL_DISCUSS_TOPIC_API + solution.slug, function(e, resp, body) {
-    if (e) return cb(e);
-    if (resp.statusCode !== 200)
-      return cb({msg: 'http error', statusCode: resp.statusCode});
-
-    var data = JSON.parse(body);
-    solution.title = data.titleRaw;
-    var $ = cheerio.load(data.posts[0].content);
-    solution.content = $.root().text();
-    return cb(null, solution);
-  });
-}
+var URL_DISCUSSES = 'https://leetcode.com/graphql';
+var URL_DISCUSS = 'https://leetcode.com/problems/$slug/discuss/$id';
 
 function getSolution(problem, lang, cb) {
   if (!problem) return cb();
 
-  request(URL_DISCUSSES + problem.discuss, function(e, resp, body) {
+  var opts = {
+    url:  URL_DISCUSSES,
+    json: true,
+    qs: {
+      query: [
+        'query fetchTopics($questionId: Int!, $pageNo: Int!, $orderBy: String!) {',
+        '  questionTopics(questionId: $questionId, pageNo: $pageNo, orderBy: $orderBy) {',
+        '    data {',
+        '      id',
+        '      title',
+        '      post {',
+        '        content',
+        '        voteCount',
+        '        author {',
+        '          username',
+        '        }',
+        '      }',
+        '    }',
+        '  }',
+        '}'
+      ].join('\n'),
+      operationName: 'fetchTopics',
+      variables: JSON.stringify({
+        pageNo:     1,
+        orderBy:    'most_votes',
+        questionId: problem.id
+      })
+    }
+  };
+  request(opts, function(e, resp, body) {
     if (e) return cb(e);
     if (resp.statusCode !== 200)
       return cb({msg: 'http error', statusCode: resp.statusCode});
@@ -51,16 +63,16 @@ function getSolution(problem, lang, cb) {
     if (lang === 'javascript') langs.push('js');
     if (lang === 'python3') langs.push('python');
 
-    var data = JSON.parse(body);
-    var solution = _.find(data.topics, function(x) {
-      var keys = x.title.toLowerCase().split(' ');
+    var solutions = body.data.questionTopics.data;
+    var solution = _.find(solutions, function(x) {
+      var keys = x.title.toLowerCase().split(/[^\w+]/);
       for (var i = 0; i < keys.length; ++i) {
         if (langs.indexOf(keys[i]) >= 0) return true;
       }
       return false;
     });
 
-    return getSolutionDetail(solution, cb);
+    return cb(null, solution);
   });
 }
 
@@ -73,16 +85,19 @@ plugin.getProblem = function(problem, cb) {
       if (e) return cb(e);
       if (!solution) return log.error('Solution not found for ' + lang);
 
+      var link = URL_DISCUSS.replace('$slug', problem.slug).replace('$id', solution.id);
+      var content = solution.post.content.replace(/\\n/g, '\n').replace(/\\t/g, '\t');
+
       log.info();
       log.info(solution.title);
       log.info();
-      log.info(chalk.underline(URL_DISCUSS_TOPIC + solution.slug));
+      log.info(chalk.underline(link));
       log.info();
       log.info('* Lang:    ' + lang);
-      log.info('* Author:  ' + solution.user.username);
-      log.info('* Votes:   ' + solution.votes);
+      log.info('* Author:  ' + solution.post.author.username);
+      log.info('* Votes:   ' + solution.post.voteCount);
       log.info();
-      log.info(solution.content);
+      log.info(content);
     });
   });
 };
