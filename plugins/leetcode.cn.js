9ec7f1d7f07cd78c982011b648b517b90ca266ae
PegasusWang <291374108@qq.com>
2022-05-11 20:07:46 +0800

    fix: change leetcode-cn.com to leetcode.cn

--- plugins/leetcode.cn.js
+++ plugins/leetcode.cn.js
@@ -17,20 +17,20 @@ var plugin = new Plugin(15, 'leetcode.cn', '2018.11.25',
 
 plugin.init = function() {
   config.app = 'leetcode.cn';
-  config.sys.urls.base            = 'https://leetcode-cn.com';
-  config.sys.urls.login           = 'https://leetcode-cn.com/accounts/login/';
-  config.sys.urls.problems        = 'https://leetcode-cn.com/api/problems/$category/';
-  config.sys.urls.problem         = 'https://leetcode-cn.com/problems/$slug/description/';
-  config.sys.urls.graphql         = 'https://leetcode-cn.com/graphql';
-  config.sys.urls.problem_detail  = 'https://leetcode-cn.com/graphql';
-  config.sys.urls.test            = 'https://leetcode-cn.com/problems/$slug/interpret_solution/';
-  config.sys.urls.session         = 'https://leetcode-cn.com/session/';
-  config.sys.urls.submit          = 'https://leetcode-cn.com/problems/$slug/submit/';
-  config.sys.urls.submissions     = 'https://leetcode-cn.com/api/submissions/$slug';
-  config.sys.urls.submission      = 'https://leetcode-cn.com/submissions/detail/$id/';
-  config.sys.urls.verify          = 'https://leetcode-cn.com/submissions/detail/$id/check/';
-  config.sys.urls.favorites       = 'https://leetcode-cn.com/list/api/questions';
-  config.sys.urls.favorite_delete = 'https://leetcode-cn.com/list/api/questions/$hash/$id';
+  config.sys.urls.base            = 'https://leetcode.cn';
+  config.sys.urls.login           = 'https://leetcode.cn/accounts/login/';
+  config.sys.urls.problems        = 'https://leetcode.cn/api/problems/$category/';
+  config.sys.urls.problem         = 'https://leetcode.cn/problems/$slug/description/';
+  config.sys.urls.graphql         = 'https://leetcode.cn/graphql';
+  config.sys.urls.problem_detail  = 'https://leetcode.cn/graphql';
+  config.sys.urls.test            = 'https://leetcode.cn/problems/$slug/interpret_solution/';
+  config.sys.urls.session         = 'https://leetcode.cn/session/';
+  config.sys.urls.submit          = 'https://leetcode.cn/problems/$slug/submit/';
+  config.sys.urls.submissions     = 'https://leetcode.cn/api/submissions/$slug';
+  config.sys.urls.submission      = 'https://leetcode.cn/submissions/detail/$id/';
+  config.sys.urls.verify          = 'https://leetcode.cn/submissions/detail/$id/check/';
+  config.sys.urls.favorites       = 'https://leetcode.cn/list/api/questions';
+  config.sys.urls.favorite_delete = 'https://leetcode.cn/list/api/questions/$hash/$id';
 };
 
 // FIXME: refactor those
@@ -89,7 +89,7 @@ plugin.getProblemsTitle = function(cb) {
 
   const opts = makeOpts(config.sys.urls.graphql);
   opts.headers.Origin = config.sys.urls.base;
-  opts.headers.Referer = 'https://leetcode-cn.com/api/problems/algorithms/';
+  opts.headers.Referer = 'https://leetcode.cn/api/problems/algorithms/';
 
   opts.json = true;
   opts.body = {
